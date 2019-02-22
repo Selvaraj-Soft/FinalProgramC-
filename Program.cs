48202d2393de49238e6724386f417c71e1bbc1d0
Selvaraj-Soft <47780683+Selvaraj-Soft@users.noreply.github.com>
2019-02-22 16:58:26 +0530

    Add File "Program"

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Continuos Integratio Topics");
+            Console.ReadLine();
+        }
+    }
+}
